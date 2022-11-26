88f50ad1be032d2c966816972060ac15d2a4f640
Attack Cristina <klimenko.an11@gmail.com>
2022-11-26 12:07:21 +0300

    day01: ex00: done

--- day01/ex00/DBReader.go
+++ day01/ex00/DBReader.go
@@ -2,82 +2,96 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"os"
 )
 
 var DBReader interface {
-	read() Cakes
-	print(cakes Cakes)
+	read() (error, Recipes)
+	print(cakes Recipes)
 	convert() string
 }
 
 type XMLname string
+type JSONname string
+type Recipes struct {
+	Cakes []struct {
+		Name        string `json:"name" xml:"name"`
+		Time        string `json:"time" xml:"stovetime"`
+		Ingredients []struct {
+			IngredientName  string `json:"ingredient_name" xml:"item>itemname"`
+			IngredientCount string `json:"ingredient_count" xml:"item>itemcount"`
+			IngredientUnit  string `json:"ingredient_unit,omitempty" xml:"item>itemunit"`
+		} `json:"ingredients" xml:"ingredients"`
+	} `json:"cake" xml:"cake"`
+}
 
-func (filename XMLname) read() Cakes {
-	fmt.Println("XMLReader")
+func (filename XMLname) read() (error, Recipes) {
+	fmt.Println("XML DBReader")
 	file, err := os.ReadFile(string(filename))
 	if err != nil {
-		fmt.Println("cannot read input file:", filename)
+		fmt.Println("cannot read input file:", filename, err)
+		return err, Recipes{nil}
 	}
 	_ = file
-	var cakes Cakes
-	return cakes
-}
-func (filename XMLname) print(cakes Cakes) {
-	fmt.Println("file xml", string(filename))
-}
-
-type JSONname string
-type Ingredients struct {
-	IngredientName  string `json:"ingredient_name"`
-	IngredientCount string `json:"ingredient_count"`
-	IngredientUnits string `json:"ingredient_unit"`
-}
-type Cake struct {
-	Name        string        `json:"name" xml:"name"`
-	Time        string        `json:"time" xml:"stovetime"`
-	Ingredients []Ingredients `json:"ingredients" xml:"ingredients"`
-}
-type Cakes struct {
-	Cakes []Cake `json:"cake" xml:"cake"`
-}
-
-func (filename JSONname) read() Cakes {
-	fmt.Println("JSONReader")
-	file, err := os.ReadFile(string(filename))
+	var cakes Recipes
+	err = xml.Unmarshal(file, &cakes)
 	if err != nil {
-		fmt.Println("cannot read input file:", filename)
+		fmt.Println("can't read parse file", err)
 	}
-	var cakes Cakes
-	err = json.Unmarshal(file, &cakes)
-	//fmt.Println("here", err, cakes.Cakes)
-
+	return err, cakes
+}
+func (filename XMLname) convert(cakes Recipes) string {
+	b, err := json.MarshalIndent(cakes, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(b)
+}
+func (filename XMLname) print(cakes Recipes) {
 	for i := 0; i < len(cakes.Cakes); i++ {
 		fmt.Println("name", cakes.Cakes[i].Name)
 		fmt.Println("time", cakes.Cakes[i].Time)
 		for j := 0; j < len(cakes.Cakes[i].Ingredients); j++ {
 			fmt.Println("\tingredient_name", cakes.Cakes[i].Ingredients[j].IngredientName)
 			fmt.Println("\tingredient_count", cakes.Cakes[i].Ingredients[j].IngredientCount)
-			fmt.Println("\tingredient_unit", cakes.Cakes[i].Ingredients[j].IngredientUnits, "\n")
+			fmt.Println("\tingredient_unit", cakes.Cakes[i].Ingredients[j].IngredientUnit, "\n")
 		}
 	}
-	return cakes
 }
-func (filename JSONname) convert(cakes Cakes) string {
-	var result string = "<rec"
-	//for cake, i := range cakes.Cakes {
-	//
-	//}
-	return result
+
+func (filename JSONname) read() (error, Recipes) {
+	fmt.Println("JSON DBReader")
+	file, err := os.ReadFile(string(filename))
+	if err != nil {
+		fmt.Println("cannot read input file:", filename)
+		return err, Recipes{}
+	}
+	var cakes Recipes
+	err = json.Unmarshal(file, &cakes)
+	if err != nil {
+		fmt.Println("can't read parse file", err)
+	}
+	return err, cakes
 }
-func (filename JSONname) print(cakes Cakes) {
+func (filename JSONname) convert(cakes Recipes) string {
+	b, err := xml.MarshalIndent(cakes, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(b)
+}
+func (filename JSONname) print(cakes Recipes) {
 	for i := 0; i < len(cakes.Cakes); i++ {
 		fmt.Println("name", cakes.Cakes[i].Name)
 		fmt.Println("time", cakes.Cakes[i].Time)
 		for j := 0; j < len(cakes.Cakes[i].Ingredients); j++ {
-			fmt.Println("ingredient_name", cakes.Cakes[i].Ingredients[j].IngredientName)
-			fmt.Println("ingredient_unit", cakes.Cakes[i].Ingredients[j].IngredientUnits)
+			fmt.Println("\tingredient_name", cakes.Cakes[i].Ingredients[j].IngredientName)
+			fmt.Println("\tingredient_count", cakes.Cakes[i].Ingredients[j].IngredientCount)
+			fmt.Println("\tingredient_unit", cakes.Cakes[i].Ingredients[j].IngredientUnit, "\n")
 		}
 	}
 }
--- day01/ex00/main.go
+++ day01/ex00/main.go
@@ -10,88 +10,24 @@ func main() {
 	var fileName *string
 	fileName = flag.String("f", "", "input file name")
 	flag.Parse()
-	fmt.Println("file name", *fileName)
 	if path.Ext(*fileName) == ".xml" {
-		content := XMLname(*fileName).read()
-
-		//_ = content
-		XMLname(*fileName).print(content)
+		err, content := XMLname(*fileName).read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//XMLname(*fileName).print(content)
+		fmt.Println(XMLname(*fileName).convert(content))
 	} else if path.Ext(*fileName) == ".json" {
-		content := JSONname(*fileName).read()
-		//JSONname(content).print()
-		_ = content
+		err, content := JSONname(*fileName).read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//JSONname(*fileName).print(content)
+		fmt.Println(JSONname(*fileName).convert(content))
 	} else {
 		fmt.Println("usage: ./readDB -f <filename>.xml")
 		fmt.Println("       ./readDB -f <filename>.json")
 	}
 }
-
-//func main() {
-//	var jsonBlob = []byte(`{
-//  "cake": [
-//    {
-//      "name": "Red Velvet Strawberry Cake",
-//      "time": "45 min",
-//      "ingredients": [
-//        {
-//          "ingredient_name": "Flour",
-//          "ingredient_count": "2",
-//          "ingredient_unit": "mugs"
-//        },
-//        {
-//          "ingredient_name": "Strawberries",
-//          "ingredient_count": "7"
-//        },
-//        {
-//          "ingredient_name": "Vanilla extract",
-//          "ingredient_count": "2.5",
-//          "ingredient_unit": "tablespoons"
-//        }
-//      ]
-//    },
-//    {
-//      "name": "Blueberry Muffin Cake",
-//      "time": "30 min",
-//      "ingredients": [
-//        {
-//          "ingredient_name": "Brown sugar",
-//          "ingredient_count": "1",
-//          "ingredient_unit": "mug"
-//        },
-//        {
-//          "ingredient_name": "Blueberries",
-//          "ingredient_count": "1",
-//          "ingredient_unit": "mug"
-//        }
-//      ]
-//    }
-//  ]
-//}`)
-//	type Ingredients struct {
-//		IngredientName  string `json:"ingredient_name"`
-//		IngredientCount string `json:"ingredient_count"`
-//		IngredientUnits string `json:"ingredient_units"`
-//	}
-//	type Cake struct {
-//		Name        string        `json:"name"`
-//		Time        string        `json:"time"`
-//		Ingredients []Ingredients `json:"ingredients"`
-//	}
-//	type Cakes struct {
-//		Cakes []Cake `json:"cake"`
-//	}
-//
-//	var cakes Cakes
-//	err := json.Unmarshal(jsonBlob, &cakes)
-//	_ = err
-//	//fmt.Println("here", err, cakes.Cakes)
-//
-//	fmt.Println(len(cakes.Cakes), err)
-//	for i := 0; i < len(cakes.Cakes); i++ {
-//		fmt.Println("name", cakes.Cakes[i].Name)
-//		fmt.Println("time", cakes.Cakes[i].Time)
-//		for j := 0; j < len(cakes.Cakes[i].Ingredients); j++ {
-//			fmt.Println("ingredient_name", cakes.Cakes[i].Ingredients[j].IngredientName)
-//		}
-//	}
-//}
