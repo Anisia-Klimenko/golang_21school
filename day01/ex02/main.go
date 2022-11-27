b420c8374ec641e050cea4f86290cf01e5e0dacc
Attack Cristina <klimenko.an11@gmail.com>
2022-11-27 10:55:48 +0300

    day01: ex02: done

--- day01/ex02/main.go
+++ day01/ex02/main.go
@@ -7,30 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
-	"sync"
 )
-
-//func main() {
-//	oldFS := flag.String("old", "", "old database")
-//	newFS := flag.String("new", "", "new database")
-//	flag.Parse()
-//
-//	if *oldFS == "" || *newFS == "" {
-//		fmt.Println("usage: ./compareFS --old snapshot1.txt --new snapshot2.txt")
-//		return
-//	}
-//
-//	new, err := os.ReadFile(string(*newFS))
-//	if err != nil {
-//		fmt.Println("cannot read input file:", *newFS, err)
-//		return
-//	}
-//	for _, line := range strings.Split(strings.TrimSuffix(string(new), "\n"), "\n") {
-//		fmt.Println(" -> ", line)
-//	}
-//}
-
-var wg sync.WaitGroup
 
 func compare(path1 string, path2 string, message string) {
 	file2, err := os.Open(path2)
@@ -44,44 +21,33 @@ func compare(path1 string, path2 string, message string) {
 	}()
 
 	s := bufio.NewScanner(file2)
-	file1, err := os.ReadFile(string(path1))
-	for _, line := range strings.Split(strings.TrimSuffix(string(file1), "\n"), "\n") {
-		//fmt.Println(" -> ", line)
-		wg.Add(1)
-		go func() {
-			for s.Scan() {
-				//fmt.Println("\t", line, " ? ", s.Text())
-				if line == s.Text() {
-					wg.Done()
-					return
-				}
+	file1, err := os.ReadFile(path1)
+	result := make(map[string]bool)
+	var found bool
+
+	for s.Scan() {
+		for _, line := range strings.Split(strings.TrimSuffix(string(file1), "\n"), "\n") {
+			line := line
+			found = false
+			if line == s.Text() {
+				found = true
+				break
 			}
-			fmt.Println(message, line)
-			wg.Done()
-		}()
+		}
+		if s.Text() != "" && !found {
+			result[s.Text()] = true
+		}
 	}
-	//for s.Scan() {
-	//	wg.Add(1)
-	//	go func() {
-	//		for _, line := range strings.Split(strings.TrimSuffix(path1, "\n"), "\n") {
-	//			time.Sleep(100)
-	//			fmt.Println("\t", line, " ? ", s.Text())
-	//			if line == s.Text() && s.Text() != "" {
-	//				wg.Done()
-	//				return
-	//			}
-	//		}
-	//		if s.Text() != "" {
-	//			fmt.Println(message, s.Text())
-	//		}
-	//		wg.Done()
-	//	}()
-	//	fmt.Println(message, " -> ", s.Text())
-	//}
-	wg.Wait()
 	err = s.Err()
 	if err != nil {
 		log.Fatal(err)
+	}
+	printResult(message, result)
+}
+
+func printResult(message string, result map[string]bool) {
+	for s := range result {
+		fmt.Println(message, s)
 	}
 }
 
@@ -95,6 +61,6 @@ func main() {
 		return
 	}
 
-	compare(*oldFS, *newFS, "ADDED")
-	//compare(*newFS, *oldFS, "REMOVED")
+	compare(*newFS, *oldFS, "ADDED")
+	compare(*oldFS, *newFS, "REMOVED")
 }
