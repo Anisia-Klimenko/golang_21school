3132e24332bbb450ba595a80ceb759b9985f60c2
Anisia Klimenko <klimenko.an11@gmail.com>
2022-11-28 11:17:37 +0300

    day02: ex02: move to iMac

--- day02/ex02/main.go
+++ day02/ex02/main.go
@@ -6,60 +6,45 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-//func main() {
-//	cmd := exec.Command("wc", "-l")
-//	//stdout, err := cmd.Output()
-//	//
-//	//if err != nil {
-//	//	fmt.Println(err.Error())
-//	//	return
-//	//}
-//	//
-//	//// Print the output
-//	//fmt.Println(string(stdout))
+//stdout, err := cmd.Output()
 //
-//	//cmd := exec.Command("cat")
-//	stdin, err := cmd.StdinPipe()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	go func() {
-//		defer stdin.Close()
-//		io.WriteString(stdin, "an old falcon")
-//	}()
-//
-//	out, err := cmd.CombinedOutput()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	fmt.Printf("%s\n", out)
+//if err != nil {
+//	fmt.Println(err.Error())
+//	return
 //}
+//
+//// Print the output
+//fmt.Println(string(stdout))
+
+//cmd := exec.Command("cat")
 
 var _ = io.WriteString
 
 func main() {
-	cmd := exec.Command("wc", "-m")
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 
-	/*
-	   stdin, err := cmd.StdinPipe()
-	   if err != nil {
-	     log.Fatal(err)
-	   }
-	   go func() {
-	     defer stdin.Close()
-	     io.WriteString(stdin, "an old falcon\n")
-	   }()
-	*/
-	cmd.Stdin = os.Stdin
+	var input []byte
+	content, _ := os.Stdin.Read(input)
+	for count, line := range strings.Split(strings.TrimSuffix(string(input), "\n"), "\n") {
+		cmd.Stdin = NewReline
 
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatal(err)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s\n", out)
 	}
 
-	fmt.Printf("%s\n", out)
+	//cmd.Stdin = os.Stdin
+	//
+	//out, err := cmd.CombinedOutput()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Printf("%s\n", out)
 }
