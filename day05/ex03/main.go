bb0b92245fc6c395287e3dd35a6adf1d8608c30f
acristin <klimenko.an11@gmail.com>
2022-11-29 09:48:48 +0300

    day05: ex03: done

--- day05/ex03/main.go
+++ day05/ex03/main.go
@@ -39,26 +39,26 @@ func appendPresent(ps []Present, p Present) []Present {
 }
 
 func grabPresents(ps []Present, capacity int) []Present {
-	//var grabbed []Present
 	if capacity < 0 {
 		fmt.Println("error: capacity less than 0")
 		return nil
 	}
+	//if len(ps) == 0 {
+	//	return ps
+	//}
 
 	table := make([][]int, len(ps)+1)
 	grabbed := make([][][]Present, len(ps)+1)
 	for i := range table {
 		table[i] = make([]int, capacity+1)
 		grabbed[i] = make([][]Present, capacity+1)
-		for j := range grabbed[i] {
-			grabbed[i][j] = make([]Present, len(ps))
-		}
 	}
+	//printTable(table)
 
 	for i := 0; i <= len(ps); i++ {
 		for j := 0; j <= capacity; j++ {
 			if i != 0 && j != 0 {
-				fmt.Println("-------- capacity", j, "--------")
+				//fmt.Println("-------- capacity", j, "--------")
 				if ps[i-1].Size > j {
 					table[i][j] = table[i-1][j]
 					grabbed[i][j] = grabbed[i-1][j]
@@ -70,24 +70,92 @@ func grabPresents(ps []Present, capacity int) []Present {
 						grabbed[i][j] = grabbed[i-1][j]
 					} else {
 						grabbed[i][j] = appendPresent(grabbed[i-1][j-ps[i-1].Size], ps[i-1])
-						//fmt.Println(grabbed[i][j])
-						fmt.Println(ps[i-1])
+						//fmt.Println(ps[i-1])
 					}
 				}
-				printTable(table)
+				//printTable(table)
 			}
-			fmt.Println(grabbed[i][j])
+			//fmt.Println(grabbed[i][j])
 		}
 	}
-
 	return grabbed[len(ps)][capacity]
 }
 
 func main() {
+	fmt.Println("---------------- all presents ----------------")
 	parray := []Present{{5, 1}, {4, 5}, {5, 2}}
 	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 5 --------")
 	grabbed := grabPresents(parray, 5)
 	if len(grabbed) != 0 {
 		printHeap(grabbed)
 	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{{4000, 4}, {2500, 1}, {2000, 3}}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 4 --------")
+	grabbed = grabPresents(parray, 4)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{{4000, 4}, {2500, 1}, {2000, 3}}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 0 --------")
+	grabbed = grabPresents(parray, 0)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println("-------- grabbed presents, capacity -1 -------")
+	grabbed = grabPresents(parray, -1)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{{5, 1}, {4, 5}, {5, 2}, {4, 6}, {4, 1},
+		{4, 2}, {3, 1}, {5, 6}}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 10 -------")
+	grabbed = grabPresents(parray, 10)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 10 -------")
+	grabbed = grabPresents(parray, 10)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{{5, 10}, {4, 50}, {5, 20}, {4, 60}, {4, 10},
+		{4, 20}, {3, 10}, {5, 60}}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 1 --------")
+	grabbed = grabPresents(parray, 1)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
+
+	fmt.Println("---------------- all presents ----------------")
+	parray = []Present{{5, 5}, {5, 5}, {5, 5}}
+	printHeap(parray)
+	fmt.Println("-------- grabbed presents, capacity 5 --------")
+	grabbed = grabPresents(parray, 5)
+	if len(grabbed) != 0 {
+		printHeap(grabbed)
+	}
+	fmt.Println()
 }
