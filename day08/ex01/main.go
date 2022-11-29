2e4befe06eb2940eab10ce6350be0958602a6807
acristin <klimenko.an11@gmail.com>
2022-11-29 18:43:46 +0300

    day08: ex01: done

--- day08/ex01/main.go
+++ day08/ex01/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 type UnknownPlant struct {
 	FlowerType string
 	LeafType   string
-	Color      int `color_scheme:"rgb"`
+	Color      int `color_scheme:"rgb" json:"name"`
 }
 
 type AnotherUnknownPlant struct {
@@ -18,17 +19,22 @@ type AnotherUnknownPlant struct {
 }
 
 func describePlant(plant interface{}) {
-	//typeOf := reflect.TypeOf(plant)
-	//tag := string(field.Tag)
-
 	v := reflect.ValueOf(plant)
 	typeS := v.Type()
 
 	for i := 0; i < v.NumField(); i++ {
-		tag := reflect.TypeOf(plant).Field(i).Tag
+		tag := string(reflect.TypeOf(plant).Field(i).Tag)
 		fmt.Printf("%s", typeS.Field(i).Name)
 		if tag != "" {
-			fmt.Printf("(%s)", tag)
+			fmt.Print("(")
+			for count, t := range strings.Split(strings.TrimSuffix(tag, " "), " ") {
+				if count != 0 {
+					fmt.Print(", ")
+				}
+				split := strings.Split(t, ":")
+				fmt.Printf("%s=%s", split[0], strings.Trim(split[1], "\""))
+			}
+			fmt.Print(")")
 		}
 		fmt.Printf(": %v\n", v.Field(i).Interface())
 	}
@@ -37,4 +43,9 @@ func describePlant(plant interface{}) {
 func main() {
 	up1 := UnknownPlant{"rose", "triangle", 12}
 	describePlant(up1)
+
+	fmt.Println()
+
+	up2 := AnotherUnknownPlant{255, "triangle", 12}
+	describePlant(up2)
 }
