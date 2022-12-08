28e21dc63a570721b3c0072cd4c7a6f61566f466
acristin <klimenko.an11@gmail.com>
2022-12-08 10:53:06 +0300

    day07: ex00: good looking tests

--- day07/ex00/minCoins2_test.go
+++ day07/ex00/minCoins2_test.go
@@ -4,152 +4,149 @@ import (
 	"reflect"
 	"sort"
 	"testing"
+	"time"
 )
 
-func TestMinCoins2(t *testing.T) {
-	for i := range coins {
-		got := minCoins2(vals[i], coins[i])
+type MinCoins2Test struct {
+	val      int
+	coins    []int
+	expected []int
+}
+
+var MinCoins2TestsEmpty = []MinCoins2Test{
+	{0, []int{1, 5, 10}, []int{}},
+	{10, []int{}, []int{}},
+	{0, []int{}, []int{}},
+}
+
+func TestMinCoins2EmptyInput(t *testing.T) {
+	for _, i := range MinCoins2TestsEmpty {
+		got := minCoins2(i.val, i.coins)
 		sort.Ints(got)
-		if !reflect.DeepEqual(got, res[i]) {
-			t.Errorf("minCoins(%d, %v) = %v, expected %v", vals[i], coins[i], got, res[i])
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
 		}
 	}
 }
 
-func TestMinCoins2EmptyVal(t *testing.T) {
-	coins := []int{1, 5, 10}
-	val := 0
-	got := minCoins2(val, coins)
-	var res []int
-	if len(got) != 0 {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
+var MinCoins2TestsNegative = []MinCoins2Test{
+	{-5, []int{1, 5, 10}, []int{}},
+	{10, []int{1, -5, 10}, []int{}},
+	{-5, []int{1, -5, 10}, []int{}},
+}
+
+func TestMinCoins2NegativeInput(t *testing.T) {
+	//go func() {
+	//	// do your testing
+	//	time.Sleep(5 * time.Second)
+	//	done <- true
+	//}()
+	//
+	//select {
+	//case <-timeout:
+	//	t.Fatal("Test didn't finish in time")
+	//case <-done:
+	//}
+	for _, i := range MinCoins2TestsNegative {
+		timeout := time.After(3 * time.Second)
+		done := make(chan []int)
+		go func() {
+			got := minCoins2(i.val, i.coins)
+			done <- got
+		}()
+		select {
+		case <-timeout:
+			t.Fatal("Test didn't finish in time")
+		case got := <-done:
+			sort.Ints(got)
+			if !reflect.DeepEqual(got, i.expected) {
+				t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+			}
+		}
 	}
 }
 
-func TestMinCoins2EmptyCoins(t *testing.T) {
-	var coins []int
-	val := 10
-	got := minCoins2(val, coins)
-	var res []int
-	if len(got) != 0 {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
+var MinCoins2TestsSorted = []MinCoins2Test{
+	{1, []int{1, 5, 10}, []int{1}},
+	{5, []int{1, 5, 10}, []int{5}},
+	{10, []int{1, 5, 10}, []int{10}},
+	{6, []int{1, 5, 10}, []int{1, 5}},
+}
+
+func TestMinCoins2Sorted(t *testing.T) {
+	for _, i := range MinCoins2TestsSorted {
+		got := minCoins2(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
 	}
 }
 
-func TestMinCoins2EmptyCoinsAndVal(t *testing.T) {
-	var coins []int
-	val := 0
-	got := minCoins2(val, coins)
-	var res []int
-	if len(got) != 0 {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
+var MinCoins2TestsUnsorted = []MinCoins2Test{
+	{6, []int{1, 10, 5}, []int{1, 5}},
+	{6, []int{5, 1, 10}, []int{1, 5}},
+	{6, []int{5, 10, 1}, []int{1, 5}},
+	{6, []int{10, 1, 5}, []int{1, 5}},
+	{6, []int{10, 5, 1}, []int{1, 5}},
+}
+
+func TestMinCoins2Unsorted(t *testing.T) {
+	for _, i := range MinCoins2TestsUnsorted {
+		got := minCoins2(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
 	}
 }
 
-func TestMinCoins2NegativeVal(t *testing.T) {
-	coins := []int{1, 5, 10}
-	val := -10
-	got := minCoins2(val, coins)
-	var res []int
-	if len(got) != 0 {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
+var MinCoins2TestsImposible = []MinCoins2Test{
+	{4, []int{5, 10, 15}, []int{}},
+	{9, []int{5, 10, 15}, []int{}},
+	{14, []int{5, 10, 15}, []int{}},
+	{16, []int{5, 10, 15}, []int{}},
+}
+
+func TestMinCoins2Imposible(t *testing.T) {
+	for _, i := range MinCoins2TestsImposible {
+		got := minCoins2(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
 	}
 }
 
-func TestMinCoins2UnsortedCoins1(t *testing.T) {
-	coins := []int{5, 1, 10}
-	val := 13
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{1, 1, 1, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
+var MinCoins2TestsDublicates = []MinCoins2Test{
+	{6, []int{1, 1, 5, 10}, []int{1, 5}},
+	{6, []int{1, 5, 1, 10}, []int{1, 5}},
+	{6, []int{1, 5, 10, 1}, []int{1, 5}},
+}
+
+func TestMinCoins2Dublicates(t *testing.T) {
+	for _, i := range MinCoins2TestsDublicates {
+		got := minCoins2(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
 	}
 }
 
-func TestMinCoins2UnsortedCoins2(t *testing.T) {
-	coins := []int{5, 10, 1}
-	val := 13
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{1, 1, 1, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
+var MinCoins2TestsOptimize = []MinCoins2Test{
+	{6, []int{4, 1, 3, 3}, []int{3, 3}},
+	{15, []int{5, 1, 5, 10}, []int{5, 10}},
+	{10, []int{1, 5, 10, 1}, []int{10}},
+	{13, []int{5, 10, 1}, []int{1, 1, 1, 10}},
 }
 
-func TestMinCoins2UnsortedCoins3(t *testing.T) {
-	coins := []int{10, 5, 1}
-	val := 13
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{1, 1, 1, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-func TestMinCoins2UnsortedCoins4(t *testing.T) {
-	coins := []int{1, 10, 5}
-	val := 10
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-
-func TestMinCoins2SortedCoins1(t *testing.T) {
-	coins := []int{1, 5, 10}
-	val := 13
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{1, 1, 1, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-
-func TestMinCoins2SortedCoins2(t *testing.T) {
-	coins := []int{1, 5, 10}
-	val := 15
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{5, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-
-func TestMinCoins2SortedCoins3(t *testing.T) {
-	coins := []int{1, 5, 10}
-	val := 10
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-
-func TestMinCoins2RecurringCoins(t *testing.T) {
-	coins := []int{5, 1, 5, 10}
-	val := 15
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{5, 10}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins2(%d, %v) = %v, expected %v", val, coins, got, res)
-	}
-}
-
-func TestMinCoins2RecurringCoins2(t *testing.T) {
-	coins := []int{4, 1, 3, 3}
-	val := 6
-	got := minCoins2(val, coins)
-	sort.Ints(got)
-	res := []int{3, 3}
-	if !reflect.DeepEqual(got, res) {
-		t.Errorf("minCoins(%d, %v) = %v, expected %v", val, coins, got, res)
+func TestMinCoins2Optimize(t *testing.T) {
+	for _, i := range MinCoins2TestsOptimize {
+		got := minCoins2(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins2(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
 	}
 }
--- day07/ex00/minCoins_test.go
+++ day07/ex00/minCoins_test.go
@@ -4,23 +4,138 @@ import (
 	"reflect"
 	"sort"
 	"testing"
+	"time"
 )
 
-var vals = []int{0, 10, 0, -10, 13, 13, 13, 10, 13, 15, 10, 15, 6}
-var coins = [][]int{
-	{1, 5, 10},
-	{},
-	{},
-	{1, 5, 10},
-	{5, 1, 10}, {5, 10, 1}, {10, 5, 1}, {1, 10, 5}, {1, 5, 10}, {1, 5, 10}, {1, 5, 10}, {5, 1, 5, 10}, {4, 1, 3, 3}}
-var res = [][]int{{}, {}, {}, {}, {1, 1, 1, 10}, {1, 1, 1, 10}, {1, 1, 1, 10}, {10}, {1, 1, 1, 10}, {5, 10}, {10}, {5, 10}, {3, 3}}
+type MinCoinsTest struct {
+	val      int
+	coins    []int
+	expected []int
+}
 
-func TestMinCoins(t *testing.T) {
-	for i := range coins {
-		got := minCoins(vals[i], coins[i])
+var MinCoinsTestsEmpty = []MinCoinsTest{
+	{0, []int{1, 5, 10}, []int{}},
+	{10, []int{}, []int{}},
+	{0, []int{}, []int{}},
+}
+
+func TestMinCoinsEmptyInput(t *testing.T) {
+	for _, i := range MinCoinsTestsEmpty {
+		got := minCoins(i.val, i.coins)
 		sort.Ints(got)
-		if !reflect.DeepEqual(got, res[i]) {
-			t.Errorf("minCoins(%d, %v) = %v, expected %v", vals[i], coins[i], got, res[i])
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
+	}
+}
+
+var MinCoinsTestsNegative = []MinCoinsTest{
+	{-5, []int{1, 5, 10}, []int{}},
+	{10, []int{1, -5, 10}, []int{}},
+	{-5, []int{1, -5, 10}, []int{}},
+}
+
+func TestMinCoinsNegativeInput(t *testing.T) {
+	for _, i := range MinCoinsTestsNegative {
+		timeout := time.After(3 * time.Second)
+		done := make(chan []int)
+		go func() {
+			got := minCoins(i.val, i.coins)
+			done <- got
+		}()
+		select {
+		case <-timeout:
+			t.Fatalf("minCoins(%d, %v) -> test didn't finish in time, endless loop", i.val, i.coins)
+		case got := <-done:
+			sort.Ints(got)
+			if !reflect.DeepEqual(got, i.expected) {
+				t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+			}
+		}
+	}
+}
+
+var MinCoinsTestsSorted = []MinCoinsTest{
+	{1, []int{1, 5, 10}, []int{1}},
+	{5, []int{1, 5, 10}, []int{5}},
+	{10, []int{1, 5, 10}, []int{10}},
+	{6, []int{1, 5, 10}, []int{1, 5}},
+}
+
+func TestMinCoinsSorted(t *testing.T) {
+	for _, i := range MinCoinsTestsSorted {
+		got := minCoins(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
+	}
+}
+
+var MinCoinsTestsUnsorted = []MinCoinsTest{
+	{6, []int{1, 10, 5}, []int{1, 5}},
+	{6, []int{5, 1, 10}, []int{1, 5}},
+	{6, []int{5, 10, 1}, []int{1, 5}},
+	{6, []int{10, 1, 5}, []int{1, 5}},
+	{6, []int{10, 5, 1}, []int{1, 5}},
+}
+
+func TestMinCoinsUnsorted(t *testing.T) {
+	for _, i := range MinCoinsTestsUnsorted {
+		got := minCoins(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
+	}
+}
+
+var MinCoinsTestsImposible = []MinCoinsTest{
+	{4, []int{5, 10, 15}, []int{}},
+	{9, []int{5, 10, 15}, []int{}},
+	{14, []int{5, 10, 15}, []int{}},
+	{16, []int{5, 10, 15}, []int{}},
+}
+
+func TestMinCoinsImposible(t *testing.T) {
+	for _, i := range MinCoinsTestsImposible {
+		got := minCoins(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
+	}
+}
+
+var MinCoinsTestsDublicates = []MinCoinsTest{
+	{6, []int{1, 1, 5, 10}, []int{1, 5}},
+	{6, []int{1, 5, 1, 10}, []int{1, 5}},
+	{6, []int{1, 5, 10, 1}, []int{1, 5}},
+}
+
+func TestMinCoinsDublicates(t *testing.T) {
+	for _, i := range MinCoinsTestsDublicates {
+		got := minCoins(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
+		}
+	}
+}
+
+var MinCoinsTestsOptimize = []MinCoinsTest{
+	{6, []int{4, 1, 3, 3, 4}, []int{3, 3}},
+	{15, []int{5, 1, 5, 10}, []int{5, 10}},
+	{10, []int{1, 5, 10, 1}, []int{10}},
+	{13, []int{5, 10, 1}, []int{1, 1, 1, 10}},
+}
+
+func TestMinCoinsOptimize(t *testing.T) {
+	for _, i := range MinCoinsTestsOptimize {
+		got := minCoins(i.val, i.coins)
+		sort.Ints(got)
+		if !reflect.DeepEqual(got, i.expected) {
+			t.Errorf("minCoins(%d, %v) = %v, expected %v", i.val, i.coins, got, i.expected)
 		}
 	}
 }
