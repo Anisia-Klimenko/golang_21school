ff14d5f5e0d23b8d454b91e3586f50e040dd54a2
acristin <klimenko.an11@gmail.com>
2022-12-09 17:59:59 +0300

    day09: ex00: done

--- day09/ex00/main.go
+++ day09/ex00/main.go
@@ -8,44 +8,26 @@ import (
 
 var wg sync.WaitGroup
 
-func sleepSort(array []int) chan int {
-	c := make(chan int)
+func sleepSort(array []int) <-chan int {
+	c := make(chan int, len(array))
 	for _, elem := range array {
+		wg.Add(1)
 		elem := elem
-		//wg.Add(1)
 		go func() {
-			time.Sleep(time.Duration(elem * 10))
-			fmt.Println("elem:", elem)
+			time.Sleep(time.Duration(elem))
 			c <- elem
-			//wg.Done()
+			wg.Done()
 		}()
-		//return c
 	}
+	wg.Wait()
+	close(c)
 	return c
-}
-
-func printInt(c chan int) {
-	fmt.Println("var:", <-c)
 }
 
 func main() {
 	array := []int{5, 3, 4, 2, 3, 1}
-	c := make(chan int, len(array))
-	go func() {
-		c = sleepSort(array)
-	}()
-	//for _ = range array {
-	//go func() {
-	//	//wg.Add(1)
-	//	printInt(c)
-	//	//wg.Done()
-	//}()
-	//}
-
-	for i := range c {
-		println(i)
+	c := sleepSort(array)
+	for d := range c {
+		fmt.Println(d)
 	}
-
-	wg.Wait()
-	close(c)
 }
