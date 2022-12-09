1f315c207ae3385a19083a8b2931d8ba84c69158
acristin <klimenko.an11@gmail.com>
2022-12-09 22:23:50 +0300

    day09: ex01, ex02: done

--- day09/ex01/main.go
+++ day09/ex01/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,60 +10,104 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 )
 
 var wg sync.WaitGroup
 
-func crawlWeb(input <-chan string) <-chan *string {
-	output := make(chan *string, len(input))
-	maxGoroutines := 8
-	guard := make(chan struct{}, maxGoroutines)
-	for url := range input {
+//func crawlWeb(input <-chan string) <-chan *string {
+//	output := make(chan *string, len(input))
+//	maxGoroutines := 8
+//	guard := make(chan struct{}, maxGoroutines)
+//	for url := range input {
+//		wg.Add(1)
+//		url := url
+//		guard <- struct{}{}
+//		go func() {
+//			resp, err := http.Get(url)
+//			if err != nil {
+//				log.Fatalln(err)
+//			}
+//			body, err := ioutil.ReadAll(resp.Body)
+//			if err != nil {
+//				log.Fatalln(err)
+//			}
+//			res := string(body)
+//			output <- &res
+//			//time.Sleep(3 * time.Second)
+//			//fmt.Println("pack")
+//			<-guard
+//			wg.Done()
+//		}()
+//	}
+//	wg.Wait()
+//	close(output)
+//	return output
+//}
+
+var mutex = sync.Mutex{}
+var cors = 0
+
+func crawlWeb(inCh <-chan string, ctx context.Context) <-chan *string {
+	guard := make(chan struct{}, 8)
+
+	outCh := make(chan *string, len(inCh))
+	defer close(outCh)
+	for url := range inCh {
+		time.Sleep(time.Duration(1 * int(time.Second)))
 		wg.Add(1)
-		url := url
-		guard <- struct{}{}
-		go func() {
-			resp, err := http.Get(url)
-			if err != nil {
-				log.Fatalln(err)
+		go func(url string) {
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				mutex.Lock()
+				cors++
+				fmt.Printf("%d goroutine started\n", cors)
+				mutex.Unlock()
+
+				time.Sleep(time.Duration(3 * int(time.Second)))
+
+				guard <- struct{}{}
+				resp, err := http.Get(url)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					log.Fatalln(err)
+				}
+				res := string(body)
+				outCh <- &res
+
+				<-guard
+				fmt.Println("goroutine ended")
 			}
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			res := string(body)
-			output <- &res
-			//time.Sleep(3 * time.Second)
-			//fmt.Println("pack")
-			<-guard
-			wg.Done()
-		}()
+		}(url)
 	}
 	wg.Wait()
-	close(output)
-	return output
+	return outCh
 }
 
 func main() {
 	size := 20
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
 	input := make(chan string, size)
 	for i := 0; i < size; i++ {
 		input <- "http://localhost:3333"
 	}
 	close(input)
 
-	//ctx, cancel := context.WithCancel(context.Background())
-	//doneChan := make(chan interface{})
-	sign := make(chan os.Signal)
-	output := crawlWeb(input)
-	signal.Notify(sign, syscall.SIGINT)
 	go func() {
-		s := <-sign
-		_ = s
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, os.Interrupt)
+		<-sigchan
 		fmt.Println("\nMy job here is done!")
-		os.Exit(0)
+		cancel()
 	}()
-	for res := range output {
-		fmt.Println(len(output), "->\t", *res)
+	outCh := crawlWeb(input, ctx)
+	for res := range outCh {
+		fmt.Println(len(outCh), "->\t", *res)
 	}
 }
